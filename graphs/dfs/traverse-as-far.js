50ac5505c020c7a9b71e236b7d87f5a1d64e90ce
ssamal2801 <sinu2290@gmail.com>
2023-10-21 22:42:43 -0400

    dfs traversal - non recursion

--- graphs/dfs/traverse-as-far.js
+++ graphs/dfs/traverse-as-far.js
@@ -9,10 +9,25 @@ const graph = {
     f: [],
 };
 
-const depthFirstPrint = ({graph, startNode}) => {
-  // Brute force solution:
-  // 1. 
-}
+const depthFirstPrint = ({ graph, startNode }) => {
+  console.log('DFS Graph Traversal - As far as possible.')
+    // Brute force solution:
+    // 1. start the stack with startNode as default and only item in it
+    // 2. pop an item, print it, that would be the current item
+    // 3. push the adjacent nodes of the current item into the stack.
+    // 4. pop again and repeat step 2 until the stack is empty.
+
+    const stack = [startNode];
+
+    while (stack.length > 0) {
+        const current = stack.pop();
+        console.log(current);
+
+        for (let node of graph[current]) {
+            stack.push(node);
+        }
+    }
+};
 
 // This method takes a graph and a starting point.
-depthFirstPrint({graph: graph, startNode: 'a'})
+depthFirstPrint({ graph: graph, startNode: 'a' });
