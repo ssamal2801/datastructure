a03cffdd94ff1ffbfc641bd2f3044e694b44e94b
ssamal94 <sinu2290@gmail.com>
2023-12-17 20:09:47 -0500

    insertion sort

--- Algorithms/Sorting/InsertionSort.js
+++ Algorithms/Sorting/InsertionSort.js
@@ -3,25 +3,30 @@
 // This sorted list is generally the left portion of the selected element.
 
 const insertionSort = (arr) => {
-  const swap = (sourceArr, smallerElementIndex, largerElementIndex) =>{
-    [sourceArr[smallerElementIndex], sourceArr[largerElementIndex]] = [sourceArr[largerElementIndex], sourceArr[smallerElementIndex]]
-  }
-  // Ascending order:
-  // Select an element starting from left of the array.
-  for(let comparingIndex = 1; comparingIndex < arr.length; comparingIndex++){
-    for(let prevIndex = 0; prevIndex < comparingIndex; prevIndex++){
-      // Compare it to each element to left of it, 
-      // if the element is greater, insert current element just before it.
-      if(arr[comparingIndex] < arr[prevIndex]){
-        swap(arr, comparingIndex, prevIndex);
-      }
+    // Ascending order:
+    // Select an element starting from left of the array.
+    for (
+        let comparingIndex = 1;
+        comparingIndex < arr.length;
+        comparingIndex++
+    ) {
+        var currentVal = arr[comparingIndex];
+        // Compare it to each element to left of it,
+        // if the element is greater, insert current element just before it.
+        for (
+            var prevIndex = comparingIndex - 1;
+            prevIndex >= 0 && currentVal < arr[prevIndex];
+            prevIndex--
+        ) {
+            arr[prevIndex + 1] = arr[prevIndex];
+        }
+        arr[prevIndex + 1] = currentVal;
     }
-  }
-  return arr;
+    return arr;
 };
 
 // Test cases
 console.log(insertionSort([9, 2, 3, 4, 5])); // [2, 3, 4, 5, 9]
 console.log(insertionSort([5, 4, 3, 2, 1])); // [1, 2, 3, 4, 5]
 // 5, 4 / i = 1 => 4 5 3 2 1
-// 5, 3 / i = 2 => 4 3 5 2 1 =>  
+// 5, 3 / i = 2 => 4 3 5 2 1 =>
